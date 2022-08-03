8c7a125cb5a3fc569e86ed19201a363897e34355
Artur Papa <artur.papa@ufv.br>
2022-08-02 23:37:32 -0300

    Up

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,6 @@
 from packages.fa.dfa import DFA
 from packages.fa.nfa import NFA
+from packages.pa.pda_modified import PDA
 
 from utils.fa.dfa_setup import setup_dfa, inputs_dfa
 from utils.fa.nfa_setup import setup_nfa, inputs_nfa
@@ -13,8 +14,33 @@ if __name__ == "__main__":
     if type == "@AFD":
         dfa = DFA()
         setup_dfa(dfa)
-        inputs_dfa(dfa)
+        dfa.display()
+        # inputs_dfa(dfa)
     elif type == "@AFN":
         nfa = NFA()
         setup_nfa(nfa)
         inputs_nfa(nfa)
+
+    elif type == "@APD":
+        pda = PDA(states={'q0', 'q1', 'q2', 'q3'},
+                  input_symbols={'a', 'b'},
+                  stack_symbols={'0', '1'},
+                  transitions={
+            'q0': {
+                # transition pushes '1' to stack
+                'a': {'0': ('q1', ('1', '0'))}
+            },
+            'q1': {
+                'a': {'1': ('q1', ('1', '1'))},
+                'b': {'1': ('q2', '')}  # transition pops from stack
+            },
+            'q2': {
+                'b': {'1': ('q2', '')},
+                '': {'0': ('q3', ('0',))}  # transition does not change stack
+            }
+        },
+            initial_state='q0',
+            initial_stack_symbol='0',
+            final_states={'q3'},
+            acceptance_mode='both')
+        
--- src/packages/pa/pda.py
+++ src/packages/pa/pda.py
@@ -1,4 +1,9 @@
 # Pushdown Automaton
+# Desempilha/Empilha
+from typing_extensions import Self
+import errors.exceptions as exceptions
+import json
+
 
 class Stack:
     def __init__(self):
@@ -7,12 +12,13 @@ class Stack:
     def __repr__(self) -> str:
         return str(self.values)
 
-    def add_value(self, value):
+    def stack(self, value):
         self.values.append(value)
 
-    def read_value(self):
-        self.values.pop()
-
+    def unstack(self, value):
+        if self.peek == value:
+            self.values.pop(value)
+    # Retorna objeto no topo da pilha
     def peek(self):
         if not self.checkEmpty():
             return self.values[-1]
@@ -23,10 +29,63 @@ class Stack:
         return False
 
 class PDA:
-    def __init__(self, sigma = set(['0', '1'])):
+    def __init__(self, sigma = set(['0', '1']),):
         self.states = set()             # Q :  conjunto de todos os estados. 
         self.sigma = sigma              # Σ :  conjunto de símbolos de entrada ou alfabeto
+        self.stack_symbols = []
         self.initial_state = None       # q0 : estado inicial.
         self.final_state = []           # F :  conjunto de estados finais
         self.transitions = dict()       # δ :  função de Transição
         self.stack = Stack()            # Γ :  conjunto de símbolos de empilhamento
+    
+    def set_states(self, states):
+        self.states = states.copy()
+    
+    def set_initial_state(self, state):
+        self.initial_state = str(state)
+    
+    def set_stack(self, stacks):
+        self.stack_symbols = stacks.copy()
+    
+    def set_final_state(self, states):
+        self.final_state = states.copy()
+
+    def set_transitions(self, transitions):
+        self.transitions = transitions
+    
+    def set_sigma(self, sigma):
+        self.sigma = sigma.copy()
+    
+    def add_final_state(self, states):
+        for state in states:
+            self.final_state.add(state)
+
+    def check_symbol_in_sigma(self, symbol):
+        if (symbol in self.sigma.values()):
+            pass
+        else:
+            raise exceptions.InvalidSymbolError('Invalid Symbol!')     
+
+
+    def transition(self, current_state, input_symbol, unstack, stack):
+        
+        if input_symbol in self.transition[current_state]:
+            self.stack.stack(stack)
+            self.stack.unstack(unstack)
+
+            return self.transitions[current_state][input_symbol]
+    
+    def input_rejection(self, current_state):
+        if current_state not in self.final_state and not self.stack.check_empty():
+            print(False) 
+
+    def current_state(self, string):
+        current_state_value = self.initial_state
+
+        if len(string.strip()) == 0:
+            return current_state_value
+        else:
+            for symbol in string:
+                self.check_symbol_in_sigma(symbol=symbol)
+                current_state_value = self.transition(current_state_value)
+
